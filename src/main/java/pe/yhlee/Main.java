207177028394a0bed299187ab2af57859595199f
Yonghee <yhlee73@gmail.com>
2014-08-18 21:31:07 +0900

    test commit

--- src/main/java/pe/yhlee/Main.java
+++
@@ -1,8 +0,0 @@
-package pe.yhlee;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/main/pe/yhlee/Main.java
@@ -0,0 +1,9 @@
+package pe.yhlee;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Mahout!!");
+        // write your code here
+    }
+}
